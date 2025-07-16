b96c77e32570b905c1267f80c9b935d04d9434b9
PangHae <yds05074@gmail.com>
2025-07-16 18:52:34 +0900

    fix: create trade log 수정

--- app/src/trade_log/services.py
+++ app/src/trade_log/services.py
@@ -20,27 +20,4 @@ def create_trade_log_service(user_id: str, body, db: Session):
     repo.insert_sentiments(trade_log_id, body.sentiments)
     db.commit()
 
-    # 전체 정보 직렬화
-    trade_log = db.query(TradeLog).filter_by(id=trade_log_id).first()
-    summary = db.query(TradeSummary).filter_by(trade_log_id=trade_log_id).first()
-    details = db.query(TradeDetail).filter_by(trade_log_id=trade_log_id).all()
-    charts = db.query(Chart).filter_by(trade_log_id=trade_log_id).all()
-    news_links = db.query(NewsLink).filter_by(trade_log_id=trade_log_id).all()
-    sentiments = db.query(TradeLogSentiment).filter_by(trade_log_id=trade_log_id).all()
-    sentiment_names = [
-        db.query(Sentiment).filter_by(id=s.sentiment_id).first().name
-        for s in sentiments
-    ]
-    return TradeLogResponseSchema(
-        id=trade_log.id,
-        user_id=trade_log.user_id,
-        date=trade_log.date,
-        summary=TradeSummarySchema(**summary.__dict__),
-        trade_details=[TradeDetailSchema(**d.__dict__) for d in details],
-        charts=[ChartSchema(**c.__dict__) for c in charts],
-        investment_type=trade_log.investment_type,
-        sentiments=sentiment_names,
-        rationale=trade_log.rationale,
-        evaluation=trade_log.evaluation,
-        news_links=[NewsLinkSchema(**n.__dict__) for n in news_links],
-    )
+    return True
